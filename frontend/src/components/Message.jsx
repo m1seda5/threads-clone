ef975ccb40858c04dc5a4ac9ca7c54ca60ee6aa1
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-08-23 13:27:51 +0300

    making a bunch of commits reversing back the appliaction isnt running for some reason

--- frontend/src/components/Message.jsx
+++ frontend/src/components/Message.jsx
@@ -1,264 +1,3 @@
-// import {
-//   Avatar,
-//   Box,
-//   IconButton,
-//   Flex,
-//   Image,
-//   Skeleton,
-//   Text,
-//   Menu,
-//   MenuButton,
-//   MenuList,
-//   MenuItem,
-// } from "@chakra-ui/react";
-// import { ChevronDownIcon } from "@chakra-ui/icons"; // Import the down icon
-// import { selectedConversationAtom } from "../atoms/messagesAtom";
-// import { useRecoilValue } from "recoil";
-// import userAtom from "../atoms/userAtom";
-// import { BsCheck2All } from "react-icons/bs";
-// import { useState } from "react";
-
-// // List of restricted words
-// const restrictedWords = [
-//   // Offensive language
-//   // (List remains unchanged)
-//   "fuck",
-//   "shit",
-//   "bitch",
-//   "cunt",
-//   "motherfucker",
-//   "asshole",
-//   "dick",
-//   "pussy",
-//   "cock",
-//   "slut",
-//   "whore",
-//   "faggot",
-//   "nigger",
-//   "chink",
-//   "gook",
-//   "spic",
-//   "raghead",
-//   "wetback",
-
-//   // Derogatory terms
-//   "retard",
-//   "cripple",
-//   "idiot",
-//   "moron",
-//   "dumbass",
-//   "lame",
-//   "loser",
-
-//   // Hate speech
-//   "terrorist",
-//   "racist",
-//   "bigot",
-//   "sexist",
-//   "homophobe",
-//   "xenophobe",
-
-//   // Insults and slurs
-//   "bastard",
-//   "scum",
-//   "pig",
-//   "skank",
-//   "tramp",
-//   "hoe",
-//   "slut",
-//   "bimbo",
-
-//   // Drugs and alcohol
-//   "crack",
-//   "heroin",
-//   "meth",
-//   "cocaine",
-//   "weed",
-//   "marijuana",
-//   "pot",
-
-//   // Sexual content
-//   "porn",
-//   "sex",
-//   "nude",
-//   "orgy",
-//   "rape",
-//   "molest",
-//   "incest",
-
-//   // Offensive phrases
-//   "go to hell",
-//   "kill yourself",
-//   "die",
-//   "you're a loser",
-//   "eat shit",
-
-//   // Additional common bad phrases
-//   "suck my dick",
-//   "blow job",
-//   "fist fuck",
-//   "cock sucking",
-//   "dickhead",
-// ];
-
-// // Function to check if a message contains any restricted words
-// const isMessageRestricted = (text) => {
-//   return restrictedWords.some((word) => text.toLowerCase().includes(word));
-// };
-
-// const Message = ({ ownMessage, message, onDelete }) => {
-//   const selectedConversation = useRecoilValue(selectedConversationAtom);
-//   const user = useRecoilValue(userAtom);
-//   const [imgLoaded, setImgLoaded] = useState(false);
-
-//   // Check if the message contains restricted words
-//   if (message.text && isMessageRestricted(message.text)) {
-//     return (
-//       <Flex justifyContent={"center"} p={2}>
-//         <Text color={"red.500"}>
-//           Message contains inappropriate content and was not sent.
-//         </Text>
-//       </Flex>
-//     );
-//   }
-
-//   return (
-//     <>
-//       {ownMessage ? (
-//         <Flex gap={2} alignSelf={"flex-end"} position="relative">
-//           {message.text && (
-//             <Flex
-//               bg={"green.800"}
-//               maxW={"350px"}
-//               p={1}
-//               borderRadius={"md"}
-//               position="relative"
-//             >
-//               {/* Start of popout menu with delete option */}
-//               <Menu>
-//                 <MenuButton
-//                   as={IconButton}
-//                   icon={<ChevronDownIcon />}
-//                   size="xs"
-//                   fontSize="10px"
-//                   variant="ghost"
-//                   colorScheme="whiteAlpha"
-//                   position="absolute"
-//                   top="-4px"
-//                   right="-4px"
-//                   borderRadius="full"
-//                   aria-label="Options"
-//                 />
-//                 <MenuList
-//                   bg={"green.800"}
-//                   backdropFilter="blur(10px)" // Adds blur to the background
-//                 >
-//                   <MenuItem onClick={() => onDelete(message._id)}>
-//                     Delete message
-//                   </MenuItem>
-//                 </MenuList>
-//               </Menu>
-//               {/* End of popout menu */}
-//               <Text color={"white"}>{message.text}</Text>
-//               <Box
-//                 alignSelf={"flex-end"}
-//                 ml={1}
-//                 color={message.seen ? "blue.400" : ""}
-//                 fontWeight={"bold"}
-//               >
-//                 <BsCheck2All size={16} />
-//               </Box>
-//             </Flex>
-//           )}
-//           {message.img && !imgLoaded && (
-//             <Flex mt={5} w={"200px"}>
-//               <Image
-//                 src={message.img}
-//                 hidden
-//                 onLoad={() => setImgLoaded(true)}
-//                 alt="Message image"
-//                 borderRadius={4}
-//               />
-//               <Skeleton w={"200px"} h={"200px"} />
-//             </Flex>
-//           )}
-//           {message.img && imgLoaded && (
-//             <Flex mt={5} w={"200px"} position="relative">
-//               <Image src={message.img} alt="Message image" borderRadius={4} />
-//               {/* Start of popout menu with delete option */}
-//               <Menu>
-//                 <MenuButton
-//                   as={IconButton}
-//                   icon={<ChevronDownIcon />}
-//                   size="2xs"
-//                   fontSize="6px"
-//                   variant="ghost"
-//                   colorScheme="whiteAlpha"
-//                   position="absolute"
-//                   top="-2px"
-//                   right="-2px"
-//                   borderRadius="full"
-//                   aria-label="Options"
-//                 />
-//                 <MenuList bg={"green.800"} backdropFilter="blur(10px)">
-//                   <MenuItem onClick={() => onDelete(message._id)}>
-//                     Delete message
-//                   </MenuItem>
-//                 </MenuList>
-//               </Menu>
-//               {/* End of popout menu */}
-//               <Box
-//                 alignSelf={"flex-end"}
-//                 ml={1}
-//                 color={message.seen ? "blue.400" : ""}
-//                 fontWeight={"bold"}
-//               >
-//                 <BsCheck2All size={16} />
-//               </Box>
-//             </Flex>
-//           )}
-//           <Avatar src={user.profilePic} w="7" h={7} />
-//         </Flex>
-//       ) : (
-//         <Flex gap={2} position="relative">
-//           <Avatar src={selectedConversation.userProfilePic} w="7" h={7} />
-//           {message.text && (
-//             <Flex
-//               position="relative"
-//               maxW={"350px"}
-//               bg={"gray.400"}
-//               p={1}
-//               borderRadius={"md"}
-//             >
-//               <Text color={"black"}>{message.text}</Text>
-//             </Flex>
-//           )}
-//           {message.img && !imgLoaded && (
-//             <Flex mt={5} w={"200px"}>
-//               <Image
-//                 src={message.img}
-//                 hidden
-//                 onLoad={() => setImgLoaded(true)}
-//                 alt="Message image"
-//                 borderRadius={4}
-//               />
-//               <Skeleton w={"200px"} h={"200px"} />
-//             </Flex>
-//           )}
-//           {message.img && imgLoaded && (
-//             <Flex mt={5} w={"200px"} position="relative">
-//               <Image src={message.img} alt="Message image" borderRadius={4} />
-//             </Flex>
-//           )}
-//         </Flex>
-//       )}
-//     </>
-//   );
-// };
-
-// export default Message;
-
-
 import {
   Avatar,
   Box,
@@ -282,6 +21,7 @@ import { useState } from "react";
 // List of restricted words
 const restrictedWords = [
   // Offensive language
+  // (List remains unchanged)
   "fuck",
   "shit",
   "bitch",
@@ -385,12 +125,7 @@ const Message = ({ ownMessage, message, onDelete }) => {
   return (
     <>
       {ownMessage ? (
-        <Flex
-          gap={2}
-          alignSelf={"flex-end"}
-          position="relative"
-          overflowX="hidden" // Hide horizontal scroll
-        >
+        <Flex gap={2} alignSelf={"flex-end"} position="relative">
           {message.text && (
             <Flex
               bg={"green.800"}
@@ -398,8 +133,6 @@ const Message = ({ ownMessage, message, onDelete }) => {
               p={1}
               borderRadius={"md"}
               position="relative"
-              overflowX="hidden" // Hide horizontal scroll
-              whiteSpace="nowrap" // Prevent text wrapping
             >
               {/* Start of popout menu with delete option */}
               <Menu>
@@ -450,7 +183,7 @@ const Message = ({ ownMessage, message, onDelete }) => {
             </Flex>
           )}
           {message.img && imgLoaded && (
-            <Flex mt={5} w={"200px"} position="relative" overflowX="hidden">
+            <Flex mt={5} w={"200px"} position="relative">
               <Image src={message.img} alt="Message image" borderRadius={4} />
               {/* Start of popout menu with delete option */}
               <Menu>
@@ -487,7 +220,7 @@ const Message = ({ ownMessage, message, onDelete }) => {
           <Avatar src={user.profilePic} w="7" h={7} />
         </Flex>
       ) : (
-        <Flex gap={2} position="relative" overflowX="hidden">
+        <Flex gap={2} position="relative">
           <Avatar src={selectedConversation.userProfilePic} w="7" h={7} />
           {message.text && (
             <Flex
@@ -496,8 +229,6 @@ const Message = ({ ownMessage, message, onDelete }) => {
               bg={"gray.400"}
               p={1}
               borderRadius={"md"}
-              overflowX="hidden" // Hide horizontal scroll
-              whiteSpace="nowrap" // Prevent text wrapping
             >
               <Text color={"black"}>{message.text}</Text>
             </Flex>
@@ -515,7 +246,7 @@ const Message = ({ ownMessage, message, onDelete }) => {
             </Flex>
           )}
           {message.img && imgLoaded && (
-            <Flex mt={5} w={"200px"} position="relative" overflowX="hidden">
+            <Flex mt={5} w={"200px"} position="relative">
               <Image src={message.img} alt="Message image" borderRadius={4} />
             </Flex>
           )}
@@ -526,3 +257,5 @@ const Message = ({ ownMessage, message, onDelete }) => {
 };
 
 export default Message;
+
+
