fd25969a915ab2365c4852bc79ce680aaf0f0557
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-08-23 21:32:34 +0300

    this reverting the erversion

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,11 +1,21 @@
 import express from "express";
 import protectRoute from "../middlewares/protectRoute.js";
-import { getMessages, sendMessage, getConversations } from "../controllers/messageController.js";
+import { getMessages, sendMessage, getConversations, deleteMessage } from "../controllers/messageController.js";
+
 
 const router = express.Router();
+
 
 router.get("/conversations", protectRoute, getConversations);
 router.get("/:otherUserId", protectRoute, getMessages);
 router.post("/", protectRoute, sendMessage);
 
+
+// Added delete message route
+router.delete("/:messageId", protectRoute, deleteMessage);
+
+
 export default router;
+
+
+
