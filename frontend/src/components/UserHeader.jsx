c8cab1d638031861d1412b0bc7afdb998a273f78
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-08-26 20:21:12 +0300

    changes made innit this is for the links to work for the video call and stuff

--- frontend/src/components/UserHeader.jsx
+++ frontend/src/components/UserHeader.jsx
@@ -1,10 +1,154 @@
+// import { Avatar } from "@chakra-ui/avatar";
+// import { Box, Flex, Link, Text, VStack } from "@chakra-ui/layout";
+// import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
+// import { Portal } from "@chakra-ui/portal";
+// import { Button, useToast } from "@chakra-ui/react";
+// import { MdMail } from "react-icons/md";
+
+// import { CgMoreO } from "react-icons/cg";
+// import { useRecoilValue } from "recoil";
+// import userAtom from "../atoms/userAtom";
+// import { Link as RouterLink } from "react-router-dom";
+// import useFollowUnfollow from "../hooks/useFollowUnfollow";
+
+// const UserHeader = ({ user }) => {
+//   const toast = useToast();
+//   const currentUser = useRecoilValue(userAtom); // logged in user
+//   const { handleFollowUnfollow, following, updating } = useFollowUnfollow(user);
+
+//   const copyURL = () => {
+//     const currentURL = window.location.href;
+//     navigator.clipboard.writeText(currentURL).then(() => {
+//       toast({
+//         title: "Success.",
+//         status: "success",
+//         description: "Profile link copied.",
+//         duration: 3000,
+//         isClosable: true,
+//       });
+//     });
+//   };
+
+//   return (
+//     <VStack gap={4} alignItems={"start"}>
+//       <Flex justifyContent={"space-between"} w={"full"}>
+//         <Box>
+//           <Text fontSize={"2xl"} fontWeight={"bold"}>
+//             {user.name}
+//           </Text>
+//           <Flex gap={2} alignItems={"center"}>
+//             <Text fontSize={"sm"}>{user.username}</Text>
+//             <Text
+//               fontSize={"xs"}
+//               bg={"gray.dark"}
+//               color={"gray.light"}
+//               p={1}
+//               borderRadius={"full"}
+//             >
+//               brookhouse
+//             </Text>
+//           </Flex>
+//         </Box>
+//         <Box>
+//           {user.profilePic && (
+//             <Avatar
+//               name={user.name}
+//               src={user.profilePic}
+//               size={{
+//                 base: "md",
+//                 md: "xl",
+//               }}
+//             />
+//           )}
+//           {!user.profilePic && (
+//             <Avatar
+//               name={user.name}
+//               src="https://bit.ly/broken-link"
+//               size={{
+//                 base: "md",
+//                 md: "xl",
+//               }}
+//             />
+//           )}
+//         </Box>
+//       </Flex>
+
+//       <Text>{user.bio}</Text>
+
+//       {currentUser?._id === user._id && (
+//         <Link as={RouterLink} to="/update">
+//           <Button size={"sm"}>Edit Profile</Button>
+//         </Link>
+//       )}
+//       {currentUser?._id !== user._id && (
+//         <Button size={"sm"} onClick={handleFollowUnfollow} isLoading={updating}>
+//           {following ? "Unfollow" : "Follow"}
+//         </Button>
+//       )}
+//       <Flex w={"full"} justifyContent={"space-between"}>
+//         <Flex gap={2} alignItems={"center"}>
+//           {/* <Text color={"gray.light"}>{user.followers.length} followers</Text> */}
+//           <Link color={"gray.light"}>Pear</Link>
+//           <Box w="1" h="1" bg={"gray.light"} borderRadius={"full"}></Box>
+//           <Link color={"gray.light"}>gmail.com</Link>
+//         </Flex>
+//         <Flex>
+//           {/* okay this is for the icon and making it more functional  
+// 					want to add an email icon that can link us to the users mail */}
+//           <Box className="icon-container">
+//             <MdMail size={24} cursor={"pointer"} />
+//           </Box>
+//           <Box className="icon-container">
+//             <Menu>
+//               <MenuButton>
+//                 <CgMoreO size={24} cursor={"pointer"} />
+//               </MenuButton>
+//               <Portal>
+//                 <MenuList bg={"gray.dark"}>
+//                   <MenuItem bg={"gray.dark"} onClick={copyURL}>
+//                     Copy link
+//                   </MenuItem>
+//                 </MenuList>
+//               </Portal>
+//             </Menu>
+//           </Box>
+//         </Flex>
+//       </Flex>
+
+//       <Flex w={"full"}>
+//         <Flex
+//           flex={1}
+//           borderBottom={"1.2px solid gray"}
+//           justifyContent={"center"}
+//           pb="3"
+//           cursor={"pointer"}
+//         >
+//           <Text fontWeight={"bold"}>Feed</Text>
+//         </Flex>
+// 		{/* <Flex
+//           flex={1}
+//           borderBottom={"1px solid gray"}
+//           justifyContent={"center"}
+//           color={"gray.light"}
+//           pb="3"
+//           cursor={"pointer"}
+//         >
+//           <Text fontWeight={"bold"}> Replies</Text>
+//         </Flex> */}
+        
+//       </Flex>
+//     </VStack>
+//   );
+// };
+
+// export default UserHeader;
+
 import { Avatar } from "@chakra-ui/avatar";
 import { Box, Flex, Link, Text, VStack } from "@chakra-ui/layout";
 import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/menu";
 import { Portal } from "@chakra-ui/portal";
 import { Button, useToast } from "@chakra-ui/react";
-import { MdMail } from "react-icons/md";
-
+import { MdVideoCall } from "react-icons/md"; // Import the video icon
 import { CgMoreO } from "react-icons/cg";
 import { useRecoilValue } from "recoil";
 import userAtom from "../atoms/userAtom";
@@ -87,16 +231,17 @@ const UserHeader = ({ user }) => {
       )}
       <Flex w={"full"} justifyContent={"space-between"}>
         <Flex gap={2} alignItems={"center"}>
-          {/* <Text color={"gray.light"}>{user.followers.length} followers</Text> */}
           <Link color={"gray.light"}>Pear</Link>
           <Box w="1" h="1" bg={"gray.light"} borderRadius={"full"}></Box>
-          <Link color={"gray.light"}>gmail.com</Link>
+          <Link href="https://pearmeet-ofadt8wim-eliammisedas-projects.vercel.app" isExternal color={"gray.light"}>
+            meet.com
+          </Link>
         </Flex>
         <Flex>
-          {/* okay this is for the icon and making it more functional  
-					want to add an email icon that can link us to the users mail */}
           <Box className="icon-container">
-            <MdMail size={24} cursor={"pointer"} />
+            <Link href="https://pearmeet-ofadt8wim-eliammisedas-projects.vercel.app" isExternal>
+              <MdVideoCall size={24} cursor={"pointer"} />
+            </Link>
           </Box>
           <Box className="icon-container">
             <Menu>
@@ -125,17 +270,6 @@ const UserHeader = ({ user }) => {
         >
           <Text fontWeight={"bold"}>Feed</Text>
         </Flex>
-		{/* <Flex
-          flex={1}
-          borderBottom={"1px solid gray"}
-          justifyContent={"center"}
-          color={"gray.light"}
-          pb="3"
-          cursor={"pointer"}
-        >
-          <Text fontWeight={"bold"}> Replies</Text>
-        </Flex> */}
-        
       </Flex>
     </VStack>
   );
