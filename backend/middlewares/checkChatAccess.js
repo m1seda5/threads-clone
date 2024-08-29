381e667c9c72b5198ad9b97b75ec3d94e4d7fb9c
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-08-29 22:40:37 +0300

    reversion

---
+++ backend/middlewares/checkChatAccess.js
@@ -0,0 +1,28 @@
+// middleware/checkChatAccess.js
+import User from "../models/userModel.js";
+
+
+const checkChatAccess = async (req, res, next) => {
+ try {
+   const userId = req.user._id; // Assuming user ID is available from the request
+   const user = await User.findById(userId);
+
+
+   if (!user) {
+     return res.status(404).json({ error: "User not found" });
+   }
+
+
+   // Check if email contains "students"
+   if (!user.email.includes("students")) {
+     // If email does not contain "students", restrict access
+     return res.status(403).json({ error: "Access to chat is restricted for your account." });
+   }
+
+
+   // Allow access if email contains "students"
+   next();
+ } catch (error) {
+   res.status(500).json({ error: error.message });
+ }
+};
