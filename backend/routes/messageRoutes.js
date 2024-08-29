8d28cb2fcb43e14b26b2719d832b44642618646a
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-08-29 23:39:38 +0300

    this is reversion of og og

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,39 +1,39 @@
-// original version before roles update
-import express from "express";
-import protectRoute from "../middlewares/protectRoute.js";
-import { getMessages, sendMessage, getConversations, deleteMessage } from "../controllers/messageController.js";
-
-
-const router = express.Router();
-
-
-router.get("/conversations", protectRoute, getConversations);
-router.get("/:otherUserId", protectRoute, getMessages);
-router.post("/", protectRoute, sendMessage);
-
-
-// Added delete message route
-router.delete("/:messageId", protectRoute, deleteMessage);
-
-
-export default router;
-
-
-// version 2 with the restrictions tings
+// // original version before roles update
 // import express from "express";
 // import protectRoute from "../middlewares/protectRoute.js";
-// import checkChatAccess from "../middlewares/checkChatAccess.js"; // Import the new middleware
 // import { getMessages, sendMessage, getConversations, deleteMessage } from "../controllers/messageController.js";
+
 
 // const router = express.Router();
 
-// // Apply both protectRoute and checkChatAccess middleware
-// router.get("/conversations", protectRoute, checkChatAccess, getConversations);
-// router.get("/:otherUserId", protectRoute, checkChatAccess, getMessages);
-// router.post("/", protectRoute, checkChatAccess, sendMessage);
+
+// router.get("/conversations", protectRoute, getConversations);
+// router.get("/:otherUserId", protectRoute, getMessages);
+// router.post("/", protectRoute, sendMessage);
+
 
 // // Added delete message route
-// router.delete("/:messageId", protectRoute, checkChatAccess, deleteMessage);
+// router.delete("/:messageId", protectRoute, deleteMessage);
+
 
 // export default router;
+
+
+// version 2
+import express from "express";
+import protectRoute from "../middlewares/protectRoute.js";
+import checkChatAccess from "../middlewares/checkChatAccess.js"; // Import the new middleware
+import { getMessages, sendMessage, getConversations, deleteMessage } from "../controllers/messageController.js";
+
+const router = express.Router();
+
+// Apply both protectRoute and checkChatAccess middleware
+router.get("/conversations", protectRoute, checkChatAccess, getConversations);
+router.get("/:otherUserId", protectRoute, checkChatAccess, getMessages);
+router.post("/", protectRoute, checkChatAccess, sendMessage);
+
+// Added delete message route
+router.delete("/:messageId", protectRoute, checkChatAccess, deleteMessage);
+
+export default router;
 
