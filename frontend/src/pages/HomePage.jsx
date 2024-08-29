6cab5d436ccea8badd83819bfc6f410b8aeecd65
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-08-29 13:26:20 +0300

    changing the home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,69 +1,4 @@
 // version 1
-import { Box, Flex, Spinner } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import useShowToast from "../hooks/useShowToast";
-import Post from "../components/Post";
-import { useRecoilState } from "recoil";
-import postsAtom from "../atoms/postsAtom";
-import SuggestedUsers from "../components/SuggestedUsers";
-
-const HomePage = () => {
-	const [posts, setPosts] = useRecoilState(postsAtom);
-	const [loading, setLoading] = useState(true);
-	const showToast = useShowToast();
-	useEffect(() => {
-		const getFeedPosts = async () => {
-			setLoading(true);
-			setPosts([]);
-			try {
-				const res = await fetch("/api/posts/feed");
-				const data = await res.json();
-				if (data.error) {
-					showToast("Error", data.error, "error");
-					return;
-				}
-				console.log(data);
-				setPosts(data);
-			} catch (error) {
-				showToast("Error", error.message, "error");
-			} finally {
-				setLoading(false);
-			}
-		};
-		getFeedPosts();
-	}, [showToast, setPosts]);
-
-	return (
-		<Flex gap='10' alignItems={"flex-start"}>
-			<Box flex={70}>
-				{!loading && posts.length === 0 && <h1>Follow users for the latest Brookhouse news.</h1>}
-
-				{loading && (
-					<Flex justify='center'>
-						<Spinner size='xl' />
-					</Flex>
-				)}
-
-				{posts.map((post) => (
-					<Post key={post._id} post={post} postedBy={post.postedBy} />
-				))}
-			</Box>
-			<Box
-				flex={30}
-				display={{
-					base: "none",
-					md: "block",
-				}}
-			>
-				<SuggestedUsers />
-			</Box>
-		</Flex>
-	);
-};
-
-export default HomePage;
-
-// version 2
 // import { Box, Flex, Spinner } from "@chakra-ui/react";
 // import { useEffect, useState } from "react";
 // import useShowToast from "../hooks/useShowToast";
@@ -76,12 +11,11 @@ export default HomePage;
 // 	const [posts, setPosts] = useRecoilState(postsAtom);
 // 	const [loading, setLoading] = useState(true);
 // 	const showToast = useShowToast();
-
 // 	useEffect(() => {
 // 		const getFeedPosts = async () => {
 // 			setLoading(true);
 // 			setPosts([]);
-// 			try {im
+// 			try {
 // 				const res = await fetch("/api/posts/feed");
 // 				const data = await res.json();
 // 				if (data.error) {
@@ -100,39 +34,20 @@ export default HomePage;
 // 	}, [showToast, setPosts]);
 
 // 	return (
-// 		<Flex gap="10" alignItems={"flex-start"}>
+// 		<Flex gap='10' alignItems={"flex-start"}>
 // 			<Box flex={70}>
-// 				{!loading && posts.length === 0 && (
-// 					<h1>Follow users for the latest Brookhouse news.</h1>
-// 				)}
+// 				{!loading && posts.length === 0 && <h1>Follow users for the latest Brookhouse news.</h1>}
 
 // 				{loading && (
-// 					<Flex justify="center">
-// 						<Spinner size="xl" />
+// 					<Flex justify='center'>
+// 						<Spinner size='xl' />
 // 					</Flex>
 // 				)}
 
 // 				{posts.map((post) => (
-// 					<Box
-// 						key={post._id}
-// 						borderWidth="1px"
-// 						borderRadius="lg"
-// 						p={4}
-// 						mb={6}
-// 						boxShadow="sm"
-// 						transition="all 0.3s ease-in-out"
-// 						_hover={{
-// 							transform: "scale(1.05) rotate(1deg)",
-// 							boxShadow: "lg",
-// 							backgroundColor: "teal.50",
-// 						}}
-// 					>
-// 						{/* Assuming Post component takes care of image and content */}
-// 						<Post post={post} postedBy={post.postedBy} />
-// 					</Box>
+// 					<Post key={post._id} post={post} postedBy={post.postedBy} />
 // 				))}
 // 			</Box>
-
 // 			<Box
 // 				flex={30}
 // 				display={{
@@ -148,4 +63,89 @@ export default HomePage;
 
 // export default HomePage;
 
-// // version 3
+// version 2
+import { Box, Flex, Spinner } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import useShowToast from "../hooks/useShowToast";
+import Post from "../components/Post";
+import { useRecoilState } from "recoil";
+import postsAtom from "../atoms/postsAtom";
+import SuggestedUsers from "../components/SuggestedUsers";
+
+const HomePage = () => {
+	const [posts, setPosts] = useRecoilState(postsAtom);
+	const [loading, setLoading] = useState(true);
+	const showToast = useShowToast();
+
+	useEffect(() => {
+		const getFeedPosts = async () => {
+			setLoading(true);
+			setPosts([]);
+			try {im
+				const res = await fetch("/api/posts/feed");
+				const data = await res.json();
+				if (data.error) {
+					showToast("Error", data.error, "error");
+					return;
+				}
+				console.log(data);
+				setPosts(data);
+			} catch (error) {
+				showToast("Error", error.message, "error");
+			} finally {
+				setLoading(false);
+			}
+		};
+		getFeedPosts();
+	}, [showToast, setPosts]);
+
+	return (
+		<Flex gap="10" alignItems={"flex-start"}>
+			<Box flex={70}>
+				{!loading && posts.length === 0 && (
+					<h1>Follow users for the latest Brookhouse news.</h1>
+				)}
+
+				{loading && (
+					<Flex justify="center">
+						<Spinner size="xl" />
+					</Flex>
+				)}
+
+				{posts.map((post) => (
+					<Box
+						key={post._id}
+						borderWidth="1px"
+						borderRadius="lg"
+						p={4}
+						mb={6}
+						boxShadow="sm"
+						transition="all 0.3s ease-in-out"
+						_hover={{
+							transform: "scale(1.05) rotate(1deg)",
+							boxShadow: "lg",
+							backgroundColor: "teal.50",
+						}}
+					>
+						{/* Assuming Post component takes care of image and content */}
+						<Post post={post} postedBy={post.postedBy} />
+					</Box>
+				))}
+			</Box>
+
+			<Box
+				flex={30}
+				display={{
+					base: "none",
+					md: "block",
+				}}
+			>
+				<SuggestedUsers />
+			</Box>
+		</Flex>
+	);
+};
+
+export default HomePage;
+
+// version 3
