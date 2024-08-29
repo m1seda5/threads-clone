b57bb643bdfa70f3ea92e1588333e19f12fb2139
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-08-30 00:10:36 +0300

    this is trying to fix context socket

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 	const user = useRecoilValue(userAtom);
 
 	useEffect(() => {
-		const socket = io("https://pear-9nh3.onrender.com/", {
+		const socket = io("/", {
 			query: {
 				userId: user?._id,
 			},
