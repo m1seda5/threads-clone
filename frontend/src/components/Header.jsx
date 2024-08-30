43459661aee8ca123118ad4ed7b7a936a07d674e
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-08-30 19:51:22 +0300

    disbaled middleware{checkchataccess} with comments and take back home page and use header to version 1 time is 19:51 appliaction is runnign

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -177,135 +177,135 @@
 
 
 // version 2 
-// import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
-// import { useRecoilValue, useSetRecoilState } from "recoil";
-// import userAtom from "../atoms/userAtom";
-// import { AiFillHome } from "react-icons/ai";
-// import { RxAvatar } from "react-icons/rx";
-// import { Link as RouterLink } from "react-router-dom";
-// import { FiLogOut } from "react-icons/fi";
-// import useLogout from "../hooks/useLogout";
-// import authScreenAtom from "../atoms/authAtom";
-// import { BsFillChatQuoteFill } from "react-icons/bs";
-// import { MdOutlineSettings } from "react-icons/md";
+import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
+import { useRecoilValue, useSetRecoilState } from "recoil";
+import userAtom from "../atoms/userAtom";
+import { AiFillHome } from "react-icons/ai";
+import { RxAvatar } from "react-icons/rx";
+import { Link as RouterLink } from "react-router-dom";
+import { FiLogOut } from "react-icons/fi";
+import useLogout from "../hooks/useLogout";
+import authScreenAtom from "../atoms/authAtom";
+import { BsFillChatQuoteFill } from "react-icons/bs";
+import { MdOutlineSettings } from "react-icons/md";
 
-// const Header = () => {
-// 	const { colorMode, toggleColorMode } = useColorMode();
-// 	const user = useRecoilValue(userAtom);
-// 	const logout = useLogout();
-// 	const setAuthScreen = useSetRecoilState(authScreenAtom);
+const Header = () => {
+	const { colorMode, toggleColorMode } = useColorMode();
+	const user = useRecoilValue(userAtom);
+	const logout = useLogout();
+	const setAuthScreen = useSetRecoilState(authScreenAtom);
 
-// 	return (
-// 		<Flex justifyContent="center" mt={6} mb="12" gap={10}>
-// 			{user && (
-// 				<Link
-// 					as={RouterLink}
-// 					to="/"
-// 					_hover={{
-// 						color: "teal.500",
-// 						transform: "scale(1.2)", // Increased scale for exaggerated effect
-// 					}}
-// 					transition="all 0.3s ease-in-out" // Smooth transition
-// 				>
-// 					<AiFillHome size={24} />
-// 				</Link>
-// 			)}
-// 			{!user && (
-// 				<Link
-// 					as={RouterLink}
-// 					to="/auth"
-// 					onClick={() => setAuthScreen("login")}
-// 					_hover={{
-// 						color: "teal.500",
-// 						transform: "scale(1.2)", // Increased scale for exaggerated effect
-// 					}}
-// 					transition="all 0.3s ease-in-out" // Smooth transition
-// 				>
-// 					Login
-// 				</Link>
-// 			)}
+	return (
+		<Flex justifyContent="center" mt={6} mb="12" gap={10}>
+			{user && (
+				<Link
+					as={RouterLink}
+					to="/"
+					_hover={{
+						color: "teal.500",
+						transform: "scale(1.2)", // Increased scale for exaggerated effect
+					}}
+					transition="all 0.3s ease-in-out" // Smooth transition
+				>
+					<AiFillHome size={24} />
+				</Link>
+			)}
+			{!user && (
+				<Link
+					as={RouterLink}
+					to="/auth"
+					onClick={() => setAuthScreen("login")}
+					_hover={{
+						color: "teal.500",
+						transform: "scale(1.2)", // Increased scale for exaggerated effect
+					}}
+					transition="all 0.3s ease-in-out" // Smooth transition
+				>
+					Login
+				</Link>
+			)}
 
-// 			<Image
-// 				cursor="pointer"
-// 				alt="logo"
-// 				w={6}
-// 				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-// 				onClick={toggleColorMode}
-// 				_hover={{
-// 					transform: "rotate(20deg) scale(1.2)", // Increased scale for exaggerated effect
-// 				}}
-// 				transition="all 0.3s ease-in-out" // Smooth transition
-// 			/>
+			<Image
+				cursor="pointer"
+				alt="logo"
+				w={6}
+				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+				onClick={toggleColorMode}
+				_hover={{
+					transform: "rotate(20deg) scale(1.2)", // Increased scale for exaggerated effect
+				}}
+				transition="all 0.3s ease-in-out" // Smooth transition
+			/>
 
-// 			{user && (
-// 				<Flex alignItems="center" gap={10}>
-// 					<Link
-// 						as={RouterLink}
-// 						to={`/${user.username}`}
-// 						_hover={{
-// 							color: "teal.500",
-// 							transform: "scale(1.2)", // Increased scale for exaggerated effect
-// 						}}
-// 						transition="all 0.3s ease-in-out" // Smooth transition
-// 					>
-// 						<RxAvatar size={24} />
-// 					</Link>
-// 					<Link
-// 						as={RouterLink}
-// 						to="/chat"
-// 						_hover={{
-// 							color: "teal.500",
-// 							transform: "scale(1.2)", // Increased scale for exaggerated effect
-// 						}}
-// 						transition="all 0.3s ease-in-out" // Smooth transition
-// 					>
-// 						<BsFillChatQuoteFill size={20} />
-// 					</Link>
-// 					<Link
-// 						as={RouterLink}
-// 						to="/settings"
-// 						_hover={{
-// 							color: "teal.500",
-// 							transform: "scale(1.2)", // Increased scale for exaggerated effect
-// 						}}
-// 						transition="all 0.3s ease-in-out" // Smooth transition
-// 					>
-// 						<MdOutlineSettings size={20} />
-// 					</Link>
-// 					<Button
-// 						size="xs"
-// 						onClick={logout}
-// 						_hover={{
-// 							bg: "teal.500",
-// 							color: "white",
-// 							transform: "scale(1.1)", // Slightly increased scale for logout button
-// 						}}
-// 						transition="all 0.3s ease-in-out" // Smooth transition
-// 					>
-// 						<FiLogOut size={20} />
-// 					</Button>
-// 				</Flex>
-// 			)}
+			{user && (
+				<Flex alignItems="center" gap={10}>
+					<Link
+						as={RouterLink}
+						to={`/${user.username}`}
+						_hover={{
+							color: "teal.500",
+							transform: "scale(1.2)", // Increased scale for exaggerated effect
+						}}
+						transition="all 0.3s ease-in-out" // Smooth transition
+					>
+						<RxAvatar size={24} />
+					</Link>
+					<Link
+						as={RouterLink}
+						to="/chat"
+						_hover={{
+							color: "teal.500",
+							transform: "scale(1.2)", // Increased scale for exaggerated effect
+						}}
+						transition="all 0.3s ease-in-out" // Smooth transition
+					>
+						<BsFillChatQuoteFill size={20} />
+					</Link>
+					<Link
+						as={RouterLink}
+						to="/settings"
+						_hover={{
+							color: "teal.500",
+							transform: "scale(1.2)", // Increased scale for exaggerated effect
+						}}
+						transition="all 0.3s ease-in-out" // Smooth transition
+					>
+						<MdOutlineSettings size={20} />
+					</Link>
+					<Button
+						size="xs"
+						onClick={logout}
+						_hover={{
+							bg: "teal.500",
+							color: "white",
+							transform: "scale(1.1)", // Slightly increased scale for logout button
+						}}
+						transition="all 0.3s ease-in-out" // Smooth transition
+					>
+						<FiLogOut size={20} />
+					</Button>
+				</Flex>
+			)}
 
-// 			{!user && (
-// 				<Link
-// 					as={RouterLink}
-// 					to="/auth"
-// 					onClick={() => setAuthScreen("signup")}
-// 					_hover={{
-// 						color: "teal.500",
-// 						transform: "scale(1.2)", // Increased scale for exaggerated effect
-// 					}}
-// 					transition="all 0.3s ease-in-out" // Smooth transition
-// 				>
-// 					Sign up
-// 				</Link>
-// 			)}
-// 		</Flex>
-// 	);
-// };
+			{!user && (
+				<Link
+					as={RouterLink}
+					to="/auth"
+					onClick={() => setAuthScreen("signup")}
+					_hover={{
+						color: "teal.500",
+						transform: "scale(1.2)", // Increased scale for exaggerated effect
+					}}
+					transition="all 0.3s ease-in-out" // Smooth transition
+				>
+					Sign up
+				</Link>
+			)}
+		</Flex>
+	);
+};
 
-// export default Header;
+export default Header;
 
 
 // version  2 with added update to roles 
@@ -440,153 +440,153 @@
 // export default Header;
 
 // version three 
-import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import userAtom from "../atoms/userAtom";
-import { AiFillHome } from "react-icons/ai";
-import { RxAvatar } from "react-icons/rx";
-import { Link as RouterLink, useNavigate } from "react-router-dom";
-import { FiLogOut } from "react-icons/fi";
-import useLogout from "../hooks/useLogout";
-import authScreenAtom from "../atoms/authAtom";
-import { BsFillChatQuoteFill } from "react-icons/bs";
-import { MdOutlineSettings } from "react-icons/md";
-import { useState } from "react";
-import { FaLock } from "react-icons/fa";
+// import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
+// import { useRecoilValue, useSetRecoilState } from "recoil";
+// import userAtom from "../atoms/userAtom";
+// import { AiFillHome } from "react-icons/ai";
+// import { RxAvatar } from "react-icons/rx";
+// import { Link as RouterLink, useNavigate } from "react-router-dom";
+// import { FiLogOut } from "react-icons/fi";
+// import useLogout from "../hooks/useLogout";
+// import authScreenAtom from "../atoms/authAtom";
+// import { BsFillChatQuoteFill } from "react-icons/bs";
+// import { MdOutlineSettings } from "react-icons/md";
+// import { useState } from "react";
+// import { FaLock } from "react-icons/fa";
 
-const Header = () => {
-	const { colorMode, toggleColorMode } = useColorMode();
-	const user = useRecoilValue(userAtom);
-	const logout = useLogout();
-	const setAuthScreen = useSetRecoilState(authScreenAtom);
-	const navigate = useNavigate();
-	const [hoveringLock, setHoveringLock] = useState(false);
+// const Header = () => {
+// 	const { colorMode, toggleColorMode } = useColorMode();
+// 	const user = useRecoilValue(userAtom);
+// 	const logout = useLogout();
+// 	const setAuthScreen = useSetRecoilState(authScreenAtom);
+// 	const navigate = useNavigate();
+// 	const [hoveringLock, setHoveringLock] = useState(false);
 
-	// Check if the user has access to the chat page based on their email
-	const hasChatAccess = user?.email?.includes("students");
+// 	// Check if the user has access to the chat page based on their email
+// 	const hasChatAccess = user?.email?.includes("students");
 
-	const handleChatClick = (e) => {
-		if (!hasChatAccess) {
-			e.preventDefault(); // Prevent navigation if the user doesn't have access
-			setHoveringLock(true); // Show red lock when hovering
-		} else {
-			setHoveringLock(false);
-			navigate("/chat");
-		}
-	};
+// 	const handleChatClick = (e) => {
+// 		if (!hasChatAccess) {
+// 			e.preventDefault(); // Prevent navigation if the user doesn't have access
+// 			setHoveringLock(true); // Show red lock when hovering
+// 		} else {
+// 			setHoveringLock(false);
+// 			navigate("/chat");
+// 		}
+// 	};
 
-	return (
-		<Flex justifyContent="center" mt={6} mb="12" gap={10}>
-			{user && (
-				<Link
-					as={RouterLink}
-					to="/"
-					_hover={{
-						color: "teal.500",
-						transform: "scale(1.2)",
-					}}
-					transition="all 0.3s ease-in-out"
-				>
-					<AiFillHome size={24} />
-				</Link>
-			)}
-			{!user && (
-				<Link
-					as={RouterLink}
-					to="/auth"
-					onClick={() => setAuthScreen("login")}
-					_hover={{
-						color: "teal.500",
-						transform: "scale(1.2)",
-					}}
-					transition="all 0.3s ease-in-out"
-				>
-					Login
-				</Link>
-			)}
+// 	return (
+// 		<Flex justifyContent="center" mt={6} mb="12" gap={10}>
+// 			{user && (
+// 				<Link
+// 					as={RouterLink}
+// 					to="/"
+// 					_hover={{
+// 						color: "teal.500",
+// 						transform: "scale(1.2)",
+// 					}}
+// 					transition="all 0.3s ease-in-out"
+// 				>
+// 					<AiFillHome size={24} />
+// 				</Link>
+// 			)}
+// 			{!user && (
+// 				<Link
+// 					as={RouterLink}
+// 					to="/auth"
+// 					onClick={() => setAuthScreen("login")}
+// 					_hover={{
+// 						color: "teal.500",
+// 						transform: "scale(1.2)",
+// 					}}
+// 					transition="all 0.3s ease-in-out"
+// 				>
+// 					Login
+// 				</Link>
+// 			)}
 
-			<Image
-				cursor="pointer"
-				alt="logo"
-				w={6}
-				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
-				onClick={toggleColorMode}
-				_hover={{
-					transform: "rotate(20deg) scale(1.2)",
-				}}
-				transition="all 0.3s ease-in-out"
-			/>
+// 			<Image
+// 				cursor="pointer"
+// 				alt="logo"
+// 				w={6}
+// 				src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+// 				onClick={toggleColorMode}
+// 				_hover={{
+// 					transform: "rotate(20deg) scale(1.2)",
+// 				}}
+// 				transition="all 0.3s ease-in-out"
+// 			/>
 
-			{user && (
-				<Flex alignItems="center" gap={10}>
-					<Link
-						as={RouterLink}
-						to={`/${user.username}`}
-						_hover={{
-							color: "teal.500",
-							transform: "scale(1.2)",
-						}}
-						transition="all 0.3s ease-in-out"
-					>
-						<RxAvatar size={24} />
-					</Link>
+// 			{user && (
+// 				<Flex alignItems="center" gap={10}>
+// 					<Link
+// 						as={RouterLink}
+// 						to={`/${user.username}`}
+// 						_hover={{
+// 							color: "teal.500",
+// 							transform: "scale(1.2)",
+// 						}}
+// 						transition="all 0.3s ease-in-out"
+// 					>
+// 						<RxAvatar size={24} />
+// 					</Link>
 
-					<Link
-						onClick={handleChatClick}
-						_hover={{
-							color: hasChatAccess ? "teal.500" : "red.500",
-							transform: hasChatAccess ? "scale(1.2)" : "scale(1.2)",
-							cursor: hasChatAccess ? "pointer" : "not-allowed",
-						}}
-						transition="all 0.3s ease-in-out"
-						onMouseEnter={() => !hasChatAccess && setHoveringLock(true)}
-						onMouseLeave={() => setHoveringLock(false)}
-					>
-						{hoveringLock ? <FaLock size={20} /> : <BsFillChatQuoteFill size={20} />}
-					</Link>
+// 					<Link
+// 						onClick={handleChatClick}
+// 						_hover={{
+// 							color: hasChatAccess ? "teal.500" : "red.500",
+// 							transform: hasChatAccess ? "scale(1.2)" : "scale(1.2)",
+// 							cursor: hasChatAccess ? "pointer" : "not-allowed",
+// 						}}
+// 						transition="all 0.3s ease-in-out"
+// 						onMouseEnter={() => !hasChatAccess && setHoveringLock(true)}
+// 						onMouseLeave={() => setHoveringLock(false)}
+// 					>
+// 						{hoveringLock ? <FaLock size={20} /> : <BsFillChatQuoteFill size={20} />}
+// 					</Link>
 
-					<Link
-						as={RouterLink}
-						to="/settings"
-						_hover={{
-							color: "teal.500",
-							transform: "scale(1.2)",
-						}}
-						transition="all 0.3s ease-in-out"
-					>
-						<MdOutlineSettings size={20} />
-					</Link>
-					<Button
-						size="xs"
-						onClick={logout}
-						_hover={{
-							bg: "teal.500",
-							color: "white",
-							transform: "scale(1.1)",
-						}}
-						transition="all 0.3s ease-in-out"
-					>
-						<FiLogOut size={20} />
-					</Button>
-				</Flex>
-			)}
+// 					<Link
+// 						as={RouterLink}
+// 						to="/settings"
+// 						_hover={{
+// 							color: "teal.500",
+// 							transform: "scale(1.2)",
+// 						}}
+// 						transition="all 0.3s ease-in-out"
+// 					>
+// 						<MdOutlineSettings size={20} />
+// 					</Link>
+// 					<Button
+// 						size="xs"
+// 						onClick={logout}
+// 						_hover={{
+// 							bg: "teal.500",
+// 							color: "white",
+// 							transform: "scale(1.1)",
+// 						}}
+// 						transition="all 0.3s ease-in-out"
+// 					>
+// 						<FiLogOut size={20} />
+// 					</Button>
+// 				</Flex>
+// 			)}
 
-			{!user && (
-				<Link
-					as={RouterLink}
-					to="/auth"
-					onClick={() => setAuthScreen("signup")}
-					_hover={{
-						color: "teal.500",
-						transform: "scale(1.2)",
-					}}
-					transition="all 0.3s ease-in-out"
-				>
-					Sign up
-				</Link>
-			)}
-		</Flex>
-	);
-};
+// 			{!user && (
+// 				<Link
+// 					as={RouterLink}
+// 					to="/auth"
+// 					onClick={() => setAuthScreen("signup")}
+// 					_hover={{
+// 						color: "teal.500",
+// 						transform: "scale(1.2)",
+// 					}}
+// 					transition="all 0.3s ease-in-out"
+// 				>
+// 					Sign up
+// 				</Link>
+// 			)}
+// 		</Flex>
+// 	);
+// };
 
-export default Header;
+// export default Header;
