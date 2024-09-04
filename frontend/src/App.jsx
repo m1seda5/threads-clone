f13b4043836ec1a4058f1b8a30a3049d163cd065
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-09-04 20:12:28 +0300

    this reverting back

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,56 +1,4 @@
 // version 1
-// import { Box, Container } from "@chakra-ui/react";
-// import { Navigate, Route, Routes, useLocation } from "react-router-dom";
-// import UserPage from "./pages/UserPage";
-// import PostPage from "./pages/PostPage";
-// import Header from "./components/Header";
-// import HomePage from "./pages/HomePage";
-// import AuthPage from "./pages/AuthPage";
-// import { useRecoilValue } from "recoil";
-// import userAtom from "./atoms/userAtom";
-// import UpdateProfilePage from "./pages/UpdateProfilePage";
-// import CreatePost from "./components/CreatePost";
-// import ChatPage from "./pages/ChatPage";
-// import { SettingsPage } from "./pages/SettingsPage";
-// function App() {
-// 	const user = useRecoilValue(userAtom);
-// 	const { pathname } = useLocation();
-// 	return (
-// 		<Box position={"relative"} w='full'>
-// 			<Container maxW={pathname === "/" ? { base: "620px", md: "900px" } : "620px"}>
-// 				<Header />
-// 				<Routes>
-// 					<Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} />
-// 					<Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
-// 					<Route path='/update' element={user ? <UpdateProfilePage /> : <Navigate to='/auth' />} />
-
-// 					<Route
-// 						path='/:username'
-// 						element={
-// 							user ? (
-// 								<>
-// 									<UserPage />
-// 									<CreatePost />
-// 								</>
-// 							) : (
-// 								<UserPage />
-// 							)
-// 						}
-// 					/>
-// 					<Route path='/:username/post/:pid' element={<PostPage />} />
-// 					<Route path='/chat' element={user ? <ChatPage /> : <Navigate to={"/auth"} />} />
-// 					<Route path='/settings' element={user ? <SettingsPage /> : <Navigate to={"/auth"} />} />
-// 				</Routes>
-// 			</Container>
-// 		</Box>
-// 	);
-// }
-
-// export default App;
-
-
-// version 2
-import React, { useEffect } from 'react';
 import { Box, Container } from "@chakra-ui/react";
 import { Navigate, Route, Routes, useLocation } from "react-router-dom";
 import UserPage from "./pages/UserPage";
@@ -64,46 +12,40 @@ import UpdateProfilePage from "./pages/UpdateProfilePage";
 import CreatePost from "./components/CreatePost";
 import ChatPage from "./pages/ChatPage";
 import { SettingsPage } from "./pages/SettingsPage";
-import i18n from "../config/i18n"; // Ensure correct import path
-
 function App() {
-  const user = useRecoilValue(userAtom);
-  const { pathname } = useLocation();
+	const user = useRecoilValue(userAtom);
+	const { pathname } = useLocation();
+	return (
+		<Box position={"relative"} w='full'>
+			<Container maxW={pathname === "/" ? { base: "620px", md: "900px" } : "620px"}>
+				<Header />
+				<Routes>
+					<Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} />
+					<Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
+					<Route path='/update' element={user ? <UpdateProfilePage /> : <Navigate to='/auth' />} />
 
-  useEffect(() => {
-    // Retrieve language from localStorage or default to English
-    const savedLanguage = localStorage.getItem("language") || "en";
-    i18n.changeLanguage(savedLanguage);
-  }, []);
-
-  return (
-    <Box position={"relative"} w="full">
-      <Container maxW={pathname === "/" ? { base: "620px", md: "900px" } : "620px"}>
-        <Header />
-        <Routes>
-          <Route path="/" element={user ? <HomePage /> : <Navigate to="/auth" />} />
-          <Route path="/auth" element={!user ? <AuthPage /> : <Navigate to="/" />} />
-          <Route path="/update" element={user ? <UpdateProfilePage /> : <Navigate to="/auth" />} />
-          <Route
-            path="/:username"
-            element={
-              user ? (
-                <>
-                  <UserPage />
-                  <CreatePost />
-                </>
-              ) : (
-                <UserPage />
-              )
-            }
-          />
-          <Route path="/:username/post/:pid" element={<PostPage />} />
-          <Route path="/chat" element={user ? <ChatPage /> : <Navigate to={"/auth"} />} />
-          <Route path="/settings" element={user ? <SettingsPage /> : <Navigate to={"/auth"} />} />
-        </Routes>
-      </Container>
-    </Box>
-  );
+					<Route
+						path='/:username'
+						element={
+							user ? (
+								<>
+									<UserPage />
+									<CreatePost />
+								</>
+							) : (
+								<UserPage />
+							)
+						}
+					/>
+					<Route path='/:username/post/:pid' element={<PostPage />} />
+					<Route path='/chat' element={user ? <ChatPage /> : <Navigate to={"/auth"} />} />
+					<Route path='/settings' element={user ? <SettingsPage /> : <Navigate to={"/auth"} />} />
+				</Routes>
+			</Container>
+		</Box>
+	);
 }
 
 export default App;
+
+
