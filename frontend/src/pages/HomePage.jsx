bf517fcff512c99ec3b6b43c78ed061d0950dc1a
Apple user <appleuser@Apples-MacBook-Pro-2.local>
2024-09-05 20:03:43 +0300

    this is going back to the homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -150,107 +150,15 @@
 // export default HomePage;
 
 // this is is home page with sugggested users disable version 3 working
-// import { Box, Flex, Spinner } from "@chakra-ui/react";
-// import { useEffect, useState } from "react";
-// import useShowToast from "../hooks/useShowToast";
-// import Post from "../components/Post";
-// import { useRecoilState } from "recoil";
-// import postsAtom from "../atoms/postsAtom";
-// // import SuggestedUsers from "../components/SuggestedUsers"; // Commented out the import
-
-// const HomePage = () => {
-// 	const [posts, setPosts] = useRecoilState(postsAtom);
-// 	const [loading, setLoading] = useState(true);
-// 	const showToast = useShowToast();
-
-// 	useEffect(() => {
-// 		const getFeedPosts = async () => {
-// 			setLoading(true);
-// 			setPosts([]);
-// 			try {
-// 				const res = await fetch("/api/posts/feed");
-// 				const data = await res.json();
-// 				if (data.error) {
-// 					showToast("Error", data.error, "error");
-// 					return;
-// 				}
-// 				console.log(data);
-// 				setPosts(data);
-// 			} catch (error) {
-// 				showToast("Error", error.message, "error");
-// 			} finally {
-// 				setLoading(false);
-// 			}
-// 		};
-// 		getFeedPosts();
-// 	}, [showToast, setPosts]);
-
-// 	return (
-// 		<Flex gap="10" alignItems={"flex-start"}>
-// 			<Box flex={70}>
-// 				{!loading && posts.length === 0 && (
-// 					<h1>welcome  to pear you have succesfully created an account login in to see the latest brookhouse news.</h1>
-// 				)}
-
-// 				{loading && (
-// 					<Flex justifyContent="center">
-// 						<Spinner size="xl" />
-// 					</Flex>
-// 				)}
-
-// 				{posts.map((post) => (
-// 					<Box
-// 						key={post._id}
-// 						borderWidth="1px"
-// 						borderRadius="lg"
-// 						p={4}
-// 						mb={6}
-// 						boxShadow="sm"
-// 						transition="all 0.3s ease-in-out"
-// 						_hover={{
-// 							transform: "scale(1.05) rotate(1deg)",
-// 							boxShadow: "lg",
-// 							backgroundColor: "teal.50",
-// 						}}
-// 					>
-// 						{/* Assuming Post component takes care of image and content */}
-// 						<Post post={post} postedBy={post.postedBy} />
-// 					</Box>
-// 				))}
-// 			</Box>
-
-// 			{/* 
-// 			<Box
-// 				flex={30}
-// 				display={{
-// 					base: "none",
-// 					md: "block",
-// 				}}
-// 			>
-// 				<SuggestedUsers />
-// 			</Box>
-// 			*/}
-// 		</Flex>
-// 	);
-// };
-
-// export default HomePage;
-
-
-
-// adding langauge change 
 import { Box, Flex, Spinner } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import useShowToast from "../hooks/useShowToast";
 import Post from "../components/Post";
 import { useRecoilState } from "recoil";
 import postsAtom from "../atoms/postsAtom";
-import { useTranslation } from 'react-i18next'; // <-- Added this line for translation
-
 // import SuggestedUsers from "../components/SuggestedUsers"; // Commented out the import
 
 const HomePage = () => {
-	const { t } = useTranslation(); // <-- Added this line for translation
 	const [posts, setPosts] = useRecoilState(postsAtom);
 	const [loading, setLoading] = useState(true);
 	const showToast = useShowToast();
@@ -281,7 +189,7 @@ const HomePage = () => {
 		<Flex gap="10" alignItems={"flex-start"}>
 			<Box flex={70}>
 				{!loading && posts.length === 0 && (
-					<h1>{t('welcome  to pear you have succesfully created an account login in to see the latest brookhouse news.')}</h1> // <-- Translated text
+					<h1>welcome  to pear you have succesfully created an account login in to see the latest brookhouse news.</h1>
 				)}
 
 				{loading && (
@@ -327,3 +235,5 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+
