abb68b2a8d7c166ea2c2ae2b403067df77129d8a
m1seda3 <orgoutsiders@gmail.com>
2024-09-15 18:20:18 -0800

    this is adding trnasltion to profile page

--- frontend/src/pages/UpdateProfilePage.jsx
+++ frontend/src/pages/UpdateProfilePage.jsx
@@ -1,3 +1,174 @@
+// version one is working 
+// import {
+// 	Button,
+// 	Flex,
+// 	FormControl,
+// 	FormLabel,
+// 	Heading,
+// 	Input,
+// 	Stack,
+// 	useColorModeValue,
+// 	Avatar,
+// 	Center,
+// } from "@chakra-ui/react";
+// import { useRef, useState } from "react";
+// import { useRecoilState } from "recoil";
+// import userAtom from "../atoms/userAtom";
+// import usePreviewImg from "../hooks/usePreviewImg";
+// import useShowToast from "../hooks/useShowToast";
+
+// export default function UpdateProfilePage() {
+// 	const [user, setUser] = useRecoilState(userAtom);
+// 	const [inputs, setInputs] = useState({
+// 		name: user.name,
+// 		username: user.username,
+// 		email: user.email,
+// 		bio: user.bio,
+// 		password: "",
+// 	});
+// 	const fileRef = useRef(null);
+// 	const [updating, setUpdating] = useState(false);
+
+// 	const showToast = useShowToast();
+
+// 	const { handleImageChange, imgUrl } = usePreviewImg();
+
+// 	const handleSubmit = async (e) => {
+// 		e.preventDefault();
+// 		if (updating) return;
+// 		setUpdating(true);
+// 		try {
+// 			const res = await fetch(`/api/users/update/${user._id}`, {
+// 				method: "PUT",
+// 				headers: {
+// 					"Content-Type": "application/json",
+// 				},
+// 				body: JSON.stringify({ ...inputs, profilePic: imgUrl }),
+// 			});
+// 			const data = await res.json(); // updated user object
+// 			if (data.error) {
+// 				showToast("Error", data.error, "error");
+// 				return;
+// 			}
+// 			showToast("Success", "Profile updated successfully", "success");
+// 			setUser(data);
+// 			localStorage.setItem("user-threads", JSON.stringify(data));
+// 		} catch (error) {
+// 			showToast("Error", error, "error");
+// 		} finally {
+// 			setUpdating(false);
+// 		}
+// 	};
+// 	return (
+// 		<form onSubmit={handleSubmit}>
+// 			<Flex align={"center"} justify={"center"} my={6}>
+// 				<Stack
+// 					spacing={4}
+// 					w={"full"}
+// 					maxW={"md"}
+// 					bg={useColorModeValue("white", "gray.dark")}
+// 					rounded={"xl"}
+// 					boxShadow={"lg"}
+// 					p={6}
+// 				>
+// 					<Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
+// 						User Profile Edit
+// 					</Heading>
+// 					<FormControl id='userName'>
+// 						<Stack direction={["column", "row"]} spacing={6}>
+// 							<Center>
+// 								<Avatar size='xl' boxShadow={"md"} src={imgUrl || user.profilePic} />
+// 							</Center>
+// 							<Center w='full'>
+// 								<Button w='full' onClick={() => fileRef.current.click()}>
+// 									Change Avatar
+// 								</Button>
+// 								<Input type='file' hidden ref={fileRef} onChange={handleImageChange} />
+// 							</Center>
+// 						</Stack>
+// 					</FormControl>
+// 					<FormControl>
+// 						<FormLabel>Full name</FormLabel>
+// 						<Input
+// 							placeholder='John Doe'
+// 							value={inputs.name}
+// 							onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
+// 							_placeholder={{ color: "gray.500" }}
+// 							type='text'
+// 						/>
+// 					</FormControl>
+// 					<FormControl>
+// 						<FormLabel>User name</FormLabel>
+// 						<Input
+// 							placeholder='johndoe'
+// 							value={inputs.username}
+// 							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+// 							_placeholder={{ color: "gray.500" }}
+// 							type='text'
+// 						/>
+// 					</FormControl>
+// 					<FormControl>
+// 						<FormLabel>Email address</FormLabel>
+// 						<Input
+// 							placeholder='your-email@example.com'
+// 							value={inputs.email}
+// 							onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+// 							_placeholder={{ color: "gray.500" }}
+// 							type='email'
+// 						/>
+// 					</FormControl>
+// 					<FormControl>
+// 						<FormLabel>Bio</FormLabel>
+// 						<Input
+// 							placeholder='Your bio.'
+// 							value={inputs.bio}
+// 							onChange={(e) => setInputs({ ...inputs, bio: e.target.value })}
+// 							_placeholder={{ color: "gray.500" }}
+// 							type='text'
+// 						/>
+// 					</FormControl>
+// 					<FormControl>
+// 						<FormLabel>Password</FormLabel>
+// 						<Input
+// 							placeholder='password'
+// 							value={inputs.password}
+// 							onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+// 							_placeholder={{ color: "gray.500" }}
+// 							type='password'
+// 						/>
+// 					</FormControl>
+// 					<Stack spacing={6} direction={["column", "row"]}>
+// 						<Button
+// 							bg={"red.400"}
+// 							color={"white"}
+// 							w='full'
+// 							_hover={{
+// 								bg: "red.500",
+// 							}}
+// 						>
+// 							Cancel
+// 						</Button>
+// 						<Button
+// 							bg={"green.400"}
+// 							color={"white"}
+// 							w='full'
+// 							_hover={{
+// 								bg: "green.500",
+// 							}}
+// 							type='submit'
+// 							isLoading={updating}
+// 						>
+// 							Submit
+// 						</Button>
+// 					</Stack>
+// 				</Stack>
+// 			</Flex>
+// 		</form>
+// 	);
+// }
+
+
+// this is version with updated 
 import {
 	Button,
 	Flex,
@@ -10,11 +181,12 @@ import {
 	Avatar,
 	Center,
 } from "@chakra-ui/react";
-import { useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useRecoilState } from "recoil";
 import userAtom from "../atoms/userAtom";
 import usePreviewImg from "../hooks/usePreviewImg";
 import useShowToast from "../hooks/useShowToast";
+import { useTranslation } from 'react-i18next'; // Import useTranslation
 
 export default function UpdateProfilePage() {
 	const [user, setUser] = useRecoilState(userAtom);
@@ -29,8 +201,22 @@ export default function UpdateProfilePage() {
 	const [updating, setUpdating] = useState(false);
 
 	const showToast = useShowToast();
-
 	const { handleImageChange, imgUrl } = usePreviewImg();
+	const { t, i18n } = useTranslation(); // Initialize the translation hook
+	const [language, setLanguage] = useState(i18n.language); // Add a state for language
+
+	// Handle language change
+	useEffect(() => {
+		const handleLanguageChange = (lng) => {
+			setLanguage(lng);
+		};
+
+		i18n.on('languageChanged', handleLanguageChange);
+
+		return () => {
+			i18n.off('languageChanged', handleLanguageChange);
+		};
+	}, [i18n]);
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
@@ -44,20 +230,21 @@ export default function UpdateProfilePage() {
 				},
 				body: JSON.stringify({ ...inputs, profilePic: imgUrl }),
 			});
-			const data = await res.json(); // updated user object
+			const data = await res.json();
 			if (data.error) {
-				showToast("Error", data.error, "error");
+				showToast(t("Error"), data.error, "error"); // Translate error message
 				return;
 			}
-			showToast("Success", "Profile updated successfully", "success");
+			showToast(t("Success"), t("Profile updated successfully"), "success"); // Translate success message
 			setUser(data);
 			localStorage.setItem("user-threads", JSON.stringify(data));
 		} catch (error) {
-			showToast("Error", error, "error");
+			showToast(t("Error"), error.message, "error"); // Translate error message
 		} finally {
 			setUpdating(false);
 		}
 	};
+
 	return (
 		<form onSubmit={handleSubmit}>
 			<Flex align={"center"} justify={"center"} my={6}>
@@ -71,7 +258,7 @@ export default function UpdateProfilePage() {
 					p={6}
 				>
 					<Heading lineHeight={1.1} fontSize={{ base: "2xl", sm: "3xl" }}>
-						User Profile Edit
+						{t("User Profile Edit")} 
 					</Heading>
 					<FormControl id='userName'>
 						<Stack direction={["column", "row"]} spacing={6}>
@@ -80,16 +267,16 @@ export default function UpdateProfilePage() {
 							</Center>
 							<Center w='full'>
 								<Button w='full' onClick={() => fileRef.current.click()}>
-									Change Avatar
+									{t("Change Avatar")} 
 								</Button>
 								<Input type='file' hidden ref={fileRef} onChange={handleImageChange} />
 							</Center>
 						</Stack>
 					</FormControl>
 					<FormControl>
-						<FormLabel>Full name</FormLabel>
+						<FormLabel>{t("Full name")}</FormLabel> 
 						<Input
-							placeholder='John Doe'
+							placeholder={t("John Doe")} 
 							value={inputs.name}
 							onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
 							_placeholder={{ color: "gray.500" }}
@@ -97,9 +284,9 @@ export default function UpdateProfilePage() {
 						/>
 					</FormControl>
 					<FormControl>
-						<FormLabel>User name</FormLabel>
+						<FormLabel>{t("User name")}</FormLabel> 
 						<Input
-							placeholder='johndoe'
+							placeholder={t("johndoe")} 
 							value={inputs.username}
 							onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
 							_placeholder={{ color: "gray.500" }}
@@ -107,9 +294,9 @@ export default function UpdateProfilePage() {
 						/>
 					</FormControl>
 					<FormControl>
-						<FormLabel>Email address</FormLabel>
+						<FormLabel>{t("Email address")}</FormLabel> 
 						<Input
-							placeholder='your-email@example.com'
+							placeholder={t("your-email@example.com")} 
 							value={inputs.email}
 							onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
 							_placeholder={{ color: "gray.500" }}
@@ -117,9 +304,9 @@ export default function UpdateProfilePage() {
 						/>
 					</FormControl>
 					<FormControl>
-						<FormLabel>Bio</FormLabel>
+						<FormLabel>{t("Bio")}</FormLabel> 
 						<Input
-							placeholder='Your bio.'
+							placeholder={t("Your bio.")} 
 							value={inputs.bio}
 							onChange={(e) => setInputs({ ...inputs, bio: e.target.value })}
 							_placeholder={{ color: "gray.500" }}
@@ -127,9 +314,9 @@ export default function UpdateProfilePage() {
 						/>
 					</FormControl>
 					<FormControl>
-						<FormLabel>Password</FormLabel>
+						<FormLabel>{t("Password")}</FormLabel>
 						<Input
-							placeholder='password'
+							placeholder={t("password")} 
 							value={inputs.password}
 							onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
 							_placeholder={{ color: "gray.500" }}
@@ -141,23 +328,19 @@ export default function UpdateProfilePage() {
 							bg={"red.400"}
 							color={"white"}
 							w='full'
-							_hover={{
-								bg: "red.500",
-							}}
+							_hover={{ bg: "red.500" }}
 						>
-							Cancel
+							{t("Cancel")} 
 						</Button>
 						<Button
 							bg={"green.400"}
 							color={"white"}
 							w='full'
-							_hover={{
-								bg: "green.500",
-							}}
+							_hover={{ bg: "green.500" }}
 							type='submit'
 							isLoading={updating}
 						>
-							Submit
+							{t("Submit")} 
 						</Button>
 					</Stack>
 				</Stack>
