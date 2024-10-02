59c44ba789b73d969d0207e541db307bb4607b9f
Miseda Eliam <misedaeliam@Misedas-MacBook-Pro.local>
2024-10-02 09:33:25 +0300

    reverting changes cause i cant debugg without my environment varibales

--- backend/middlewares/protectRoute.js
+++ backend/middlewares/protectRoute.js
@@ -24,32 +24,6 @@
 
 
 // adding debugging additional requser
-// import User from "../models/userModel.js";
-// import jwt from "jsonwebtoken";
-
-// const protectRoute = async (req, res, next) => {
-//   try {
-//     const token = req.cookies.jwt;
-
-//     if (!token) return res.status(401).json({ message: "Unauthorized" });
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-//     const user = await User.findById(decoded.userId).select("-password");
-
-//     if (!user) return res.status(404).json({ error: "User not found" });
-
-//     req.user = user;
-
-//     next();
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// };
-
-// export default protectRoute;
-
-// actuall debugging wiht the console and the back end 
 import User from "../models/userModel.js";
 import jwt from "jsonwebtoken";
 
@@ -57,25 +31,18 @@ const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies.jwt;
 
-    if (!token) {
-      console.log("No token found");
-      return res.status(401).json({ message: "Unauthorized" });
-    }
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Decoded userId:", decoded.userId);
 
     const user = await User.findById(decoded.userId).select("-password");
-    if (!user) {
-      console.log("User not found in the database");
-      return res.status(404).json({ error: "User not found" });
-    }
+
+    if (!user) return res.status(404).json({ error: "User not found" });
 
     req.user = user;
 
     next();
   } catch (err) {
-    console.error("Error in protectRoute middleware:", err);
     res.status(500).json({ message: err.message });
   }
 };
